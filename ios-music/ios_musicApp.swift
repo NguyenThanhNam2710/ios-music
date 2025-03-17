bacbf1559dae1dc0a2fcc869c2955dc51e4c1bb7
NamNT97 <thanhnam2k27@gmail.com>
2025-03-17 11:20:18 +0700

    Initial Commit

---
+++ ios-music/ios_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_musicApp.swift
+//  ios-music
+//
+//  Created by NamNT97 on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
